8598a649e8c7ec22ec08edac4a083f84ec83600a
JDMTias <matiasiturbide@gmail.com>
2020-09-10 19:04:52 -0600

    h2 config added pom file checked and application properties added

---
+++ src/test/java/com/lambdaschool/getorders2/Getorders2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.getorders2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Getorders2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
